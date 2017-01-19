1f1ee52ae229de99c43582ba1e20e1e58ab622b9
youshanmomei <1142041743@qq.com>
2017-01-19 17:08:28 +0800

    sp-03 modify Employees ok.

--- IspringmvcTest03/src/main/java/org/hy/handlers/EmployeeHandler.java
+++ IspringmvcTest03/src/main/java/org/hy/handlers/EmployeeHandler.java
@@ -5,9 +5,7 @@ import org.hy.dao.EmployeeDao;
 import org.hy.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -23,6 +21,30 @@ public class EmployeeHandler {
     @Autowired
     private DepartmentDao departmentDao;
 
+    /**
+     * the method is help for the method of update
+     */
+    @ModelAttribute
+    public void getEmployee(@RequestParam(value = "id", required = false) Integer id, Map<String, Object> map){
+        if (id != null) {
+            map.put("employee", employeeDao.get(id));
+        }
+    }
+
+    @RequestMapping(value = "/emp", method = RequestMethod.PUT)
+    public String update(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+
+
+    @RequestMapping(value = "/emp/{id}", method = RequestMethod.GET)
+    public String input(@PathVariable("id") Integer id, Map<String, Object> map) {
+        map.put("employee", employeeDao.get(id));
+        map.put("departments", departmentDao.getDepartments());
+        return "input";
+    }
 
     @RequestMapping(value = "/emp/{id}", method = RequestMethod.DELETE)
     public String delete(@PathVariable("id") Integer id){
